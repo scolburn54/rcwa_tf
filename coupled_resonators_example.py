9b2112a5fea279c283003773f6ee89a8e32d02db
Ubuntu <ubuntu@ip-172-31-22-31.us-west-2.compute.internal>
2020-03-27 13:58:18 +0000

    Switched to uniform initialization

--- coupled_resonators_example.py
+++ coupled_resonators_example.py
@@ -17,15 +17,15 @@ def focal_spot():
   focal_plane = solver.propagate(params['input'] * field, params)
   index = (params['pixelsX'] * params['upsample']) // 2
   f1 = tf.abs(focal_plane[0, index, index])
-  f2 = tf.abs(focal_plane[1, index, index])
-  loss = -f1 * f2
+  #f2 = tf.abs(focal_plane[1, index, index])
+  loss = -f1
 
   return loss
 
 # Initialize global params dictionary and overwrite default values.
 setup_t0 = time.time()
 params = solver.initialize_params()
-params['batchSize'] = 2
+params['batchSize'] = 1
 params['erd'] = 6.76
 params['ers'] = 2.25
 params['PQ'] = [3, 3]
@@ -42,17 +42,17 @@ params['Nx'] = Nx
 Ny = 128
 params['Ny'] = Ny
 params['sigmoid_coeff'] = 1000.0
-params['upsample'] = 5
+params['upsample'] = 11
 
 # Define the batch parameters and duty cycle variable.
 simulation_shape = (batchSize, pixelsX, pixelsY)
 batch_shape = (batchSize, pixelsX, pixelsY, 1, 1, 1)
 pol_shape = (batchSize, pixelsX, pixelsY, 1)
-lam0 = params['nanometers'] * tf.convert_to_tensor([633.0, 530.0], dtype = tf.float32)
+lam0 = params['nanometers'] * tf.convert_to_tensor([633.0], dtype = tf.float32)
 lam0 = lam0[:, tf.newaxis, tf.newaxis, tf.newaxis, tf.newaxis, tf.newaxis]
 lam0 = tf.tile(lam0, multiples = (1, pixelsX, pixelsY, 1, 1, 1))
 params['lam0'] = lam0
-theta = params['degrees'] * tf.convert_to_tensor([0.0, 0.0], dtype = tf.float32)
+theta = params['degrees'] * tf.convert_to_tensor([0.0], dtype = tf.float32)
 theta = theta[:, tf.newaxis, tf.newaxis, tf.newaxis, tf.newaxis, tf.newaxis]
 theta = tf.tile(theta, multiples = (1, pixelsX, pixelsY, 1, 1, 1))
 params['theta'] = theta
@@ -63,9 +63,8 @@ params['propagator'] = solver.make_propagator(params)
 params['input'] = solver.define_input_fields(params)
 
 var_shape = (batchSize, pixelsX, pixelsY, 4)
-np.random.RandomState(seed = 0)
-r_x_initial = np.random.normal(loc = 0.125, scale = 0.025, size = var_shape)
-r_y_initial = np.random.normal(loc = 0.125, scale = 0.025, size = var_shape)
+r_x_initial = 0.125 * np.ones(shape = var_shape)
+r_y_initial = r_x_initial
 r_x_var = tf.Variable(r_x_initial, dtype = tf.float32)
 r_y_var = tf.Variable(r_y_initial, dtype = tf.float32)
 
@@ -99,8 +98,8 @@ print('Loss: ' + str(loss[N]))
 
 # Simulate the system.
 outputs = solver.simulate(epsilon_r_initial, mu_r_initial, params)
-field = outputs['ty'][:, :, :, np.prod(params['PQ']) // 2, 0]
-focal_plane_initial = solver.propagate(field, params)
+field_initial = outputs['ty'][:, :, :, np.prod(params['PQ']) // 2, 0]
+focal_plane_initial = solver.propagate(field_initial, params)
 
 ER_t, UR_t = solver.generate_coupled_cylindrical_resonators(r_x_var, r_y_var, params)
 
@@ -113,6 +112,8 @@ focal_plane = solver.propagate(field, params)
 np.savetxt('loss.txt', loss)
 np.save('focal_plane_initial.npy', focal_plane_initial)
 np.save('focal_plane_opt.npy', focal_plane)
+np.save('field_initial.npy', field_initial)
+np.save('field_opt.npy', field)
 np.save('r_x_initial.npy', r_x_initial)
 np.save('r_y_initial.npy', r_y_initial)
 np.save('r_x_final.npy', r_x_var.numpy())
