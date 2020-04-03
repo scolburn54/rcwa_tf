aa628ee6a52d768adfedaa70bd4c682997a80e70
Ubuntu <ubuntu@ip-172-31-46-216.us-west-2.compute.internal>
2020-04-03 15:18:53 +0000

    Updated for higher truncation order

--- coupled_resonators_example.py
+++ coupled_resonators_example.py
@@ -17,18 +17,18 @@ def focal_spot():
   focal_plane = solver.propagate(params['input'] * field, params)
   index = (params['pixelsX'] * params['upsample']) // 2
   f1 = tf.abs(focal_plane[0, index, index])
-  #f2 = tf.abs(focal_plane[1, index, index])
-  loss = -f1
+  f2 = tf.abs(focal_plane[1, index, index])
+  loss = -f1 * f2
 
   return loss
 
 # Initialize global params dictionary and overwrite default values.
 setup_t0 = time.time()
 params = solver.initialize_params()
-params['batchSize'] = 1
+params['batchSize'] = 2
 params['erd'] = 6.76
 params['ers'] = 2.25
-params['PQ'] = [3, 3]
+params['PQ'] = [5, 5]
 params['f'] = 15E-6
 batchSize = params['batchSize']
 num_pixels = 31
@@ -48,11 +48,11 @@ params['upsample'] = 11
 simulation_shape = (batchSize, pixelsX, pixelsY)
 batch_shape = (batchSize, pixelsX, pixelsY, 1, 1, 1)
 pol_shape = (batchSize, pixelsX, pixelsY, 1)
-lam0 = params['nanometers'] * tf.convert_to_tensor([633.0], dtype = tf.float32)
+lam0 = params['nanometers'] * tf.convert_to_tensor([633.0, 633.0], dtype = tf.float32)
 lam0 = lam0[:, tf.newaxis, tf.newaxis, tf.newaxis, tf.newaxis, tf.newaxis]
 lam0 = tf.tile(lam0, multiples = (1, pixelsX, pixelsY, 1, 1, 1))
 params['lam0'] = lam0
-theta = params['degrees'] * tf.convert_to_tensor([0.0], dtype = tf.float32)
+theta = params['degrees'] * tf.convert_to_tensor([0.0, 0.0], dtype = tf.float32)
 theta = theta[:, tf.newaxis, tf.newaxis, tf.newaxis, tf.newaxis, tf.newaxis]
 theta = tf.tile(theta, multiples = (1, pixelsX, pixelsY, 1, 1, 1))
 params['theta'] = theta
@@ -62,8 +62,8 @@ params['ptm'] = 0 * tf.ones(shape = pol_shape, dtype = tf.complex64)
 params['propagator'] = solver.make_propagator(params)
 params['input'] = solver.define_input_fields(params)
 
-var_shape = (batchSize, pixelsX, pixelsY, 4)
-r_x_initial = 0.125 * np.ones(shape = var_shape)
+var_shape = (1, pixelsX, pixelsY, 4)
+r_x_initial = 0.175 * np.ones(shape = var_shape)
 r_y_initial = r_x_initial
 r_x_var = tf.Variable(r_x_initial, dtype = tf.float32)
 r_y_var = tf.Variable(r_y_initial, dtype = tf.float32)
@@ -72,10 +72,10 @@ r_y_var = tf.Variable(r_y_initial, dtype = tf.float32)
 epsilon_r_initial, mu_r_initial = solver.generate_coupled_cylindrical_resonators(r_x_var, r_y_var, params)
 
 # Number of optimization iterations.
-N = 10000
+N = 112
 
 # Define an optimizer and data to be stored.
-opt = tf.keras.optimizers.Adam(learning_rate = 1E-3)
+opt = tf.keras.optimizers.Adam(learning_rate = 5E-4)
 loss = np.zeros(N + 1)
 duty = np.zeros(N + 1)
 setup_time = time.time() - setup_t0
@@ -86,10 +86,16 @@ print('Loss: ' + str(loss[0]))
 print('\nOptimizing...')
 
 # Optimize.
+prev_ten_percent = 0
+curr_ten_percent = 0
 t = time.time()
 for i in range(N):
   opt.minimize(focal_spot, var_list = [r_x_var, r_y_var])
   loss[i + 1] = focal_spot().numpy()
+  prev_ten_percent = curr_ten_percent
+  curr_ten_percent = np.int(10.0 * i / N)
+  if (curr_ten_percent != prev_ten_percent):
+      print(str(10 * curr_ten_percent) + '% complete')
 
 elapsed_time = time.time() - t
 print('Setup Time: ' + str(setup_time))
